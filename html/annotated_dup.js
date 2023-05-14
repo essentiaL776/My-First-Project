94d4e0c9537d054b7c75a774086981050eef351c
Edvinas <114932724+essentiaL776@users.noreply.github.com>
2023-05-14 23:46:04 +0300

    v2.0

---
+++ html/annotated_dup.js
@@ -0,0 +1,6 @@
+var annotated_dup =
+[
+    [ "Person", "class_person.html", "class_person" ],
+    [ "Student", "class_student.html", "class_student" ],
+    [ "Timer", "class_timer.html", "class_timer" ]
+];
---
+++ html/files_dup.js
@@ -0,0 +1,7 @@
+var files_dup =
+[
+    [ "main.cpp", "main_8cpp.html", "main_8cpp" ],
+    [ "mylib.cpp", "mylib_8cpp.html", "mylib_8cpp" ],
+    [ "mylib.h", "mylib_8h.html", "mylib_8h" ],
+    [ "student.h", "student_8h.html", "student_8h" ]
+];
